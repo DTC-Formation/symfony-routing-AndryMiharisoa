b737be790250a265f8c0ddeb04c6acc609d4da26
AndryMihary <miharyandrianjafimanana@gmail.com>
2023-06-29 16:30:24 +0200

    cread

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -6,14 +6,15 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('Nom')
-            ->add('Prenom')
+            ->add('Nom', TextType::class,['label' => 'Nom'])
+            ->add('Prenom', TextType::class,['label' => 'Prénom'])
             ->add('Age')
             ->add('Cin')
             ->add('Adresse')
